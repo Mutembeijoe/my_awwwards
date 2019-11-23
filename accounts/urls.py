b9beba7d8124a403b0dcf318e80162cddf606035
mutembeijoe <mutembeijoe@gmail.com>
2019-11-23 21:18:17 +0300

    adds views for User Profile and User creation

---
+++ accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import UserDetailView
+
+urlpatterns = [
+    path('user/<int:pk>', UserDetailView.as_view(), name='profile')
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from .models import CustomUser
 
 # Create your views here.
+
+
+class UserDetailView(DetailView):
+    model = CustomUser
+    template_name = 'profile.html'
+
+
+class UserCreateView(CreateView):
+    model = CustomUser
+    template_name="signup.html"
+    fields = ('username', 'email', 'telephone', 'bio')
