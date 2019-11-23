3a0a3092599a6aa4c7ddd1d6139c1b38d31093b2
mutembeijoe <mutembeijoe@gmail.com>
2019-11-23 15:14:28 +0300

    adds a customer user model that extends Abstract user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    avatar = models.ImageField(upload_to='avatars', default='default_avatar.jpg')
+    bio = models.CharField(max_length=500)
+    telephone = models.CharField(max_length=13)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+        
