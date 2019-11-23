c31fac835ca7720788a97dade3f8973443279b32
mutembeijoe <mutembeijoe@gmail.com>
2019-11-23 15:27:40 +0300

    makes bio and telephone  field in custom user model not required by default

--- accounts/models.py
+++ accounts/models.py
@@ -5,10 +5,8 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     avatar = models.ImageField(upload_to='avatars', default='default_avatar.jpg')
-    bio = models.CharField(max_length=500)
-    telephone = models.CharField(max_length=13)
-    country = models.CharField(max_length=100)
+    bio = models.CharField(max_length=500, null=True, blank=True)
+    telephone = models.CharField(max_length=13, blank=True, null=True) 
 
     def __str__(self):
         return self.username
-        
