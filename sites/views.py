5b23b01ffaae1e88cc7e314368342d228dd347dc
mutembeijoe <mutembeijoe@gmail.com>
2019-11-24 12:24:34 +0300

    adds a Sites list View and configures it as the home page

--- sites/views.py
+++ sites/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Site
 
 # Create your views here.
+
+
+class SiteListView(ListView):
+    model = Site
+    template_name = 'home.html'
