2301255b56a9a089040e0c5e6acbfb8f95504cc5
mutembeijoe <mutembeijoe@gmail.com>
2019-11-24 11:46:58 +0300

    adds Rating, Site and Tags models in admin

--- sites/admin.py
+++ sites/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Site, Rating, Tag
 # Register your models here.
+
+admin.site.register(Site)
+admin.site.register(Rating)
+admin.site.register(Tag)
