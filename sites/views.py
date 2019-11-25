fa342e7ce075b39322901e195b1d8243ac340e31
mutembeijoe <mutembeijoe@gmail.com>
2019-11-25 07:35:17 +0300

    adds  percentage meters ro display the site taings

--- sites/views.py
+++ sites/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.db.models import Sum
 from .models import Site
 
 # Create your views here.
@@ -14,3 +15,15 @@ class SiteListView(ListView):
 class SiteDetailView(DetailView):
     model = Site
     template_name= 'site.html'
+    def get_context_data(self, **kwargs):          
+        context = super().get_context_data(**kwargs)                     
+        site = Site.objects.get(id=self.object.id)
+        count = len(site.rating.all())
+        content = (site.rating.all().aggregate(Sum('content')).get('content__sum'))/count * 10
+        design = (site.rating.all().aggregate(Sum('design')).get('design__sum'))/count *10
+        usability = (site.rating.all().aggregate(Sum('usability')).get('usability__sum'))/count *10
+        context["content"] = int(content)
+        context["design"] = int(design)
+        context["usability"] = int(usability)
+        context["count"] = count
+        return context
