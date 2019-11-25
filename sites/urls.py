186cc0fb5cc6a93797f93d70fe8b06a7c49edf2c
mutembeijoe <mutembeijoe@gmail.com>
2019-11-25 08:16:15 +0300

    adds a site create view and template form with crispy forms

--- sites/urls.py
+++ sites/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SiteListView, SiteDetailView
+from .views import SiteListView, SiteDetailView, SiteCreateView
 
 urlpatterns = [
     path('', SiteListView.as_view(), name='home'),
+    path('site/new/', SiteCreateView.as_view(), name='add_site'),
     path('site/<int:pk>', SiteDetailView.as_view(), name='site')
 ]
--- sites/views.py
+++ sites/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from django.db.models import Sum
 from .models import Site
 
 # Create your views here.
+ 
+class SiteCreateView(CreateView):
+    model = Site
+    template_name = 'add_site.html'
+    fields = ('cover','title','description','tags')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class SiteListView(ListView):
